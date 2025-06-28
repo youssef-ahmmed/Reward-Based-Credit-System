21105b167ac0479ef72eb0488f73bd98323ed503
Yousef Ahmed <115221628+youssef-ahmmed@users.noreply.github.com>
2025-06-28 06:05:33 +0300

    Update db init to be connection pool

--- internal/shared/database/db.go
+++ internal/shared/database/db.go
@@ -5,34 +5,53 @@ import (
 	"gorm.io/gorm/schema"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func InitDB() *gorm.DB {
+var (
+	db   *gorm.DB
+	once sync.Once
+)
+
+func init() {
 	_ = godotenv.Load()
+}
 
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s search_path=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_SCHEMA"),
-	)
+func GetDB() *gorm.DB {
+	once.Do(func() {
+		var err error
+		dsn := fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%s search_path=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_SCHEMA"),
+		)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true,
-		},
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: true,
+			},
+		})
+		if err != nil {
+			log.Fatalf("❌ Failed to connect to database: %v", err)
+		}
+
+		sqlDB, err := db.DB()
+		if err != nil {
+			log.Fatalf("❌ Could not get DB instance: %v", err)
+		}
+
+		sqlDB.SetMaxIdleConns(10)
+		sqlDB.SetMaxOpenConns(100)
+		sqlDB.SetConnMaxLifetime(0)
 	})
-
-	if err != nil {
-		log.Fatalf("Failed to connect to DB: %v", err)
-	}
 
 	return db
 }
